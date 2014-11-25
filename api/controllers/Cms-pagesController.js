9ef95ef93f96e84369d6a3d687b220cdc4bb1d9f
sbchen8 <sbchens@walla.com>
2014-11-26 00:02:06 +0200

    first version

---
+++ api/controllers/Cms-pagesController.js
@@ -0,0 +1,11 @@
+/**
+ * Cms-pagesController
+ *
+ * @description :: Server-side logic for managing cms-pages
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
